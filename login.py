ecbab0075d684cd07aa671325371b3254deed7d6
harshada jadhav <harshadajadhav1611@gmail.com>
2026-06-16 08:42:52 -0500

    Login code added

---
+++ login.py
@@ -0,0 +1,6 @@
+username = input('Enter username: ')
+password = input('Enter password: ')
+if username =='Ram' and password =='Ram@123':
+    print('Valid username and password')
+else:
+    print('Invalid username and password')    
--- range.py
+++ range.py
@@ -1,10 +1,12 @@
 # r=range(10)
 # print(type(r))
+# print(r)
 # for x in r:
 #     print(x)
 
 
 r=range(1,10,2)
 print(type(r))
+print(r)
 for x in r:
     print(x)
