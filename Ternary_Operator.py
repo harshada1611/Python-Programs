3ecb88822d526b96a207d401d73b169a670eea71
harshada jadhav <harshadajadhav1611@gmail.com>
2026-06-19 08:59:10 -0500

    ternary operator code added

--- Ternary_Operator.py
+++ Ternary_Operator.py
@@ -8,8 +8,17 @@
 
 
 #Maximun value
+# a = int(input('Enter value for a: '))
+# b = int(input('Enter value for b: '))
+# c = int(input('Enter value for c: '))
+# max = a if a>b and a>c else b if b>c else c
+# print('The maximum value is: ',max)
+
+
+#Equal,Smaller,Greter
 a = int(input('Enter value for a: '))
 b = int(input('Enter value for b: '))
-c = int(input('Enter value for c: '))
-max = a if a>b and a>c else b if b>c else c
-print('The maximum value is: ',max)
+
+result = 'equal' if a == b else 'smaller' if a < b else 'greater'
+
+print('The result is:', result)
