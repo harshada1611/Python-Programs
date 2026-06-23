4335ac31fe1862e9329bf8f6165aff3dc1136503
harshada jadhav <harshadajadhav1611@gmail.com>
2026-06-23 08:26:57 -0500

    Added code in ternary_operator

--- Ternary_Operator.py
+++ Ternary_Operator.py
@@ -16,9 +16,59 @@
 
 
 #Equal,Smaller,Greter
-a = int(input('Enter value for a: '))
-b = int(input('Enter value for b: '))
+# a = int(input('Enter value for a: '))
+# b = int(input('Enter value for b: '))
 
-result = 'equal' if a == b else 'smaller' if a < b else 'greater'
+# result = 'equal' if a == b else 'smaller' if a < b else 'greater'
 
-print('The result is:', result)
+# print('The result is:', result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Greter
+# x= input('Enter value for x: ')
+# y= input('Enter value for y: ')
+# z= input('Enter value for z: ')
+# result =  x if x > y else y if y > z else z;
+# print('REsult is: ',result)
+
+#Smaller
+# x= input('Enter value for x: ')
+# y= input('Enter value for y: ')
+# z= input('Enter value for z: ')
+# result =  x if x < y else y if y < z else z;
+# print('REsult is: ',result)
+
+#Equal
+x= input('Enter value for x: ')
+y= input('Enter value for y: ')
+result = "equl" if x == y else "greter" if x > y else "smaller"
+print('Result is: ',result)
