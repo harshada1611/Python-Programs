548a941e255021b0fc599c8ee132f40fd9f9b116
harshada jadhav <harshadajadhav1611@gmail.com>
2026-06-27 20:56:57 -0500

    added data

---
+++ Sep_End_operator.py
@@ -0,0 +1,16 @@
+#Sep_End_operator
+
+# a = 10
+# b = 20
+# c = 30
+# print(a,b,c,sep = ":" ,end = "*")
+# print("Ram", end = "+")
+# print("Dadu", end = "=")
+# print("Brothers")
+
+
+A=1000
+B=2000
+print(A,B, sep = ":",end="=")
+print('hello',end='+')
+print('Bye')
---
+++ replacement_operator.py
@@ -0,0 +1,7 @@
+# replacement_operator
+name = 'laukik'
+salary = 90000
+friend = 'rahul'
+# print('Hello {} your salary is {} your friend is happy for you'.format(name,salary,friend))
+print('Hello {0} your salary is {1} your friend {2} is happy for you'.format(0,1,2))
+
