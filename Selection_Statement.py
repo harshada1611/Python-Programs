8dc9f05176d520d77245c510671d0176c1c8527c
harshada jadhav <harshadajadhav1611@gmail.com>
2026-06-28 19:57:30 -0500

    added data

---
+++ Selection_Statement.py
@@ -0,0 +1,23 @@
+# Selection_Statement
+# 1. if
+# 2. if_else
+
+# 1.if
+# x = 9
+# if (x < 10):
+#     print('X is less than 10')
+# print('Out from program')   
+
+
+# name = input('Enter name: ')
+# if (name == "Laukik"):
+#     print('Hello Laukik welcome')
+# print('Have a good day')    
+
+# 2.If_else
+name = input('Print name: ')
+if(name == 'Ram'):
+    print("Hello Ram")
+else:
+    print('Hello Guest')
+print('Have a good day')        
