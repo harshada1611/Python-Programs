a91d40f6f09306c319845d5a65e8c64f0616fce2
harshada jadhav <harshadajadhav1611@gmail.com>
2026-06-30 08:39:04 -0500

    Added if_elif_else code

---
+++ If_else.py
@@ -0,0 +1,16 @@
+# If_else
+
+# name = input('Enter name: ')
+# if name == 'Ram':
+#     print('Hello Ram')
+# else:
+#     print('Hello Guest')    
+# print('Welcome')    
+
+
+# Even_Odd
+num =  int(input('Enter number: '))
+if num%2==0:
+    print('Even number')
+else:
+    print('Odd number')    
---
+++ if_elif_else.py
@@ -0,0 +1,11 @@
+# if_elif_else
+
+brand = input('Enter your fav brand: ')
+if brand == 'Addidas':
+    print('Shoe')
+elif brand =='Rollex':
+    print('Watch') 
+elif brand == 'Redtape':
+    print('Jacket')
+else:
+    print('Not your brand here')       
